55e82d24c21033881cab1fa35b794b3980f9edd4
Dmitrii Chudinov <chudinov.d@selectel.com>
2024-02-07 16:05:50 +0300

    Support DNSv2

--- main_test.go
+++ main_test.go
@@ -4,20 +4,28 @@ import (
 	"os"
 	"testing"
 
-	"github.com/jetstack/cert-manager/test/acme/dns"
+	acmetest "github.com/cert-manager/cert-manager/test/acme"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 var zone = os.Getenv("TEST_ZONE_NAME")
 
 func TestRunsSuite(t *testing.T) {
+	t.Parallel()
+	// We must setup logger
+	// https://pkg.go.dev/sigs.k8s.io/controller-runtime/pkg/log#pkg-variables
+	// example from https://sdk.operatorframework.io/docs/building-operators/golang/references/logging/
+	logger := zap.New()
+	logf.SetLogger(logger)
 	// The manifest path should contain a file named config.json that is a
 	// snippet of valid configuration that should be included on the
 	// ChallengeRequest passed as part of the test cases.
-	fixture := dns.NewFixture(&selectelDNSProviderSolver{},
-		dns.SetResolvedZone(zone),
-		dns.SetAllowAmbientCredentials(false),
-		dns.SetManifestPath("testdata/selectel"),
-		dns.SetStrict(true),
+	fixture := acmetest.NewFixture(&selectelDNSProviderSolver{},
+		acmetest.SetResolvedZone(zone),
+		acmetest.SetAllowAmbientCredentials(false),
+		acmetest.SetManifestPath("testdata/selectel"),
+		acmetest.SetStrict(true),
 	)
 	fixture.RunConformance(t)
 }
--- selectel/selectel.go
+++ selectel/selectel.go
@@ -1,195 +1,220 @@
 // Package selectel implements a DNS provider for solving the DNS-01 challenge using Selectel Domains API.
-// Selectel Domain API reference: https://kb.selectel.com/23136054.html
-// Token: https://my.selectel.ru/profile/apikeys
+// Selectel Domain API v2 reference: https://developers.selectel.ru/docs/cloud-services/dns_api/dns_api_actual/
 package selectel
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
-	"os"
-	"strconv"
 	"time"
 
 	"github.com/selectel/cert-manager-webhook-selectel/selectel/internal"
+	domainsV2 "github.com/selectel/domains-go/pkg/v2"
+	"github.com/selectel/go-selvpcclient/v3/selvpcclient"
 )
 
 const (
-	defaultBaseURL            = "https://api.selectel.ru/domains/v1"
-	minTTL                    = 60
-	defaultPropagationTimeout = 120
-	defaultPollingInterval    = 2
-	defaultHTTPTimeout        = 30
+	defaultBaseURL     = "https://api.selectel.ru/domains/v2"
+	minTTL             = 60
+	defaultHTTPTimeout = 40
+
+	userAgent               = "cert-manager-webhook-selectel"
+	headerForOSProjectToken = "X-Auth-Token"
 )
 
-const (
-	envNamespace             = "SELECTEL_"
-	baseURLEnvVar            = envNamespace + "BASE_URL"
-	apiTokenEnvVar           = envNamespace + "API_TOKEN"
-	ttlEnvVar                = envNamespace + "TTL"
-	propagationTimeoutEnvVar = envNamespace + "PROPAGATION_TIMEOUT"
-	pollingIntervalEnvVar    = envNamespace + "POLLING_INTERVAL"
-	httpTimeoutEnvVar        = envNamespace + "HTTP_TIMEOUT"
-)
-
-const (
-	errConfigAbsent       = "the configuration of the DNS provider is absent"
-	errCredentialsMissing = "credentials missing"
+var (
+	errTTLMustBeGreaterOrEqualsMinTTL = fmt.Errorf("ttl must be greater or equals min ttl: %d", minTTL)
+	errConfigAbsent                   = fmt.Errorf("the configuration of the DNS provider is absent")
+	errKeyNotFoundInSecret            = fmt.Errorf("key not found in secret")
 )
 
 // Config is used to configure the creation of the DNSProvider.
 type Config struct {
-	BaseURL            string
-	Token              string
-	PropagationTimeout time.Duration
-	PollingInterval    time.Duration
-	TTL                int
-	HTTPClient         *http.Client
+	BaseURL           string            `json:"baseUrl"`
+	TTL               int               `json:"ttl"`
+	HTTPTimeout       int               `json:"httpTimeout"`
+	CredentialsForDNS CredentialsForDNS `json:"-"`
 }
 
-// NewDefaultConfig returns a default configuration for the DNSProvider.
-func NewDefaultConfig() *Config {
-	return &Config{
-		BaseURL:            getEnvOrDefaultString(baseURLEnvVar, defaultBaseURL),
-		TTL:                getEnvOrDefaultInt(ttlEnvVar, minTTL),
-		PropagationTimeout: getEnvOrDefaultSecond(propagationTimeoutEnvVar, defaultPropagationTimeout),
-		PollingInterval:    getEnvOrDefaultSecond(pollingIntervalEnvVar, defaultPollingInterval),
-		HTTPClient: &http.Client{
-			Timeout: getEnvOrDefaultSecond(httpTimeoutEnvVar, defaultHTTPTimeout),
-		},
-	}
+type CredentialsForDNS struct {
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	AccountID string `json:"account_id"`
+	ProjectID string `json:"project_id"`
 }
 
-// DNSProvider is an implementation of the acme.ChallengeProvider interface.
-type DNSProvider struct {
-	config *Config
-	client *internal.Client
-}
-
-// NewDNSProvider returns a DNSProvider instance configured for Selectel Domains API.
-// API token must be passed in the environment variable SELECTEL_API_TOKEN.
-func NewDNSProvider() (*DNSProvider, error) {
-	token, err := getEnvOrErrorString(apiTokenEnvVar)
+func (credentials *CredentialsForDNS) FromMapBytes(dataFromSecret map[string][]byte) error {
+	var err error
+	credentials.Username, err = getValueFromMapIfIsNotEmpty(dataFromSecret, "username")
 	if err != nil {
-		return nil, fmt.Errorf("selectel: %v", err)
+		return err
 	}
-
-	config := NewDefaultConfig()
-	config.Token = token
-
-	return NewDNSProviderConfig(config)
-}
-
-// NewDNSProviderConfig return a DNSProvider instance configured for selectel.
-func NewDNSProviderConfig(config *Config) (*DNSProvider, error) {
-	if config == nil {
-		return nil, fmt.Errorf("selectel: %s", errConfigAbsent)
-	}
-
-	if config.Token == "" {
-		return nil, fmt.Errorf("selectel: %s", errCredentialsMissing)
-	}
-
-	if config.TTL < minTTL {
-		return nil, fmt.Errorf("selectel: invalid TTL, TTL (%d) must be greater than %d",
-			config.TTL,
-			minTTL)
-	}
-
-	return &DNSProvider{
-		config: config,
-		client: internal.NewClient(internal.ClientOpts{
-			BaseURL:    config.BaseURL,
-			Token:      config.Token,
-			HTTPClient: config.HTTPClient,
-		}),
-	}, nil
-}
-
-// Timeout returns the Timeout and interval to use when checking for DNS propagation.
-// Adjusting here to cope with spikes in propagation times.
-func (d *DNSProvider) Timeout() (timeout, interval time.Duration) {
-	return d.config.PropagationTimeout, d.config.PollingInterval
-}
-
-// Present creates a TXT record to fulfill DNS-01 challenge.
-func (d *DNSProvider) Present(fqdn, value string) error {
-	domainObj, err := d.client.GetDomainByName(fqdn)
+	credentials.Password, err = getValueFromMapIfIsNotEmpty(dataFromSecret, "password")
 	if err != nil {
-		return fmt.Errorf("selectel: %v", err)
+		return err
 	}
-
-	txtRecord := internal.Record{
-		Type:    "TXT",
-		TTL:     d.config.TTL,
-		Name:    fqdn,
-		Content: value,
-	}
-	_, err = d.client.AddRecord(domainObj.ID, txtRecord)
+	credentials.AccountID, err = getValueFromMapIfIsNotEmpty(dataFromSecret, "account_id")
 	if err != nil {
-		return fmt.Errorf("selectel: %v", err)
+		return err
+	}
+	credentials.ProjectID, err = getValueFromMapIfIsNotEmpty(dataFromSecret, "project_id")
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
-// CleanUp removes a TXT record used for DNS-01 challenge.
-func (d *DNSProvider) CleanUp(fqdn, key string) error {
-	domainObj, err := d.client.GetDomainByName(fqdn)
-	if err != nil {
-		return fmt.Errorf("selectel: %v", err)
+func getValueFromMapIfIsNotEmpty(dataFromSecret map[string][]byte, key string) (string, error) {
+	value, ok := dataFromSecret[key]
+	if !ok && len(value) == 0 {
+		return "", errKeyNotFoundInSecret
 	}
 
-	records, err := d.client.ListRecords(domainObj.ID)
-	if err != nil {
-		return fmt.Errorf("selectel: %v", err)
+	return string(value), nil
+}
+
+// NewDefaultConfig returns a default configuration for the DNSProvider.
+func NewConfigForDNS() (*Config, error) {
+	cfg := &Config{
+		BaseURL:     defaultBaseURL,
+		TTL:         minTTL,
+		HTTPTimeout: defaultHTTPTimeout,
 	}
 
-	// Delete records with specific FQDN and key
-	var lastErr error
-	for _, record := range records {
-		if record.Name == unFQDN(fqdn) && record.Content == key {
-			err = d.client.DeleteRecord(domainObj.ID, record.ID)
-			if err != nil {
-				lastErr = fmt.Errorf("selectel: %v", err)
-			}
+	return cfg, nil
+}
+
+// DNSProvider is an implementation of the acme.ChallengeProvider interface.
+type DNSProvider struct {
+	config    *Config
+	dnsClient domainsV2.DNSClient[domainsV2.Zone, domainsV2.RRSet]
+}
+
+// NewDNSProviderConfig return a DNSProvider instance configured for selectel.
+func NewDNSProviderFromConfig(config *Config) (*DNSProvider, error) {
+	if config == nil {
+		return nil, errConfigAbsent
+	}
+	if config.TTL < minTTL {
+		return nil, errTTLMustBeGreaterOrEqualsMinTTL
+	}
+
+	dnsClient, err := getDNSClientFromConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DNSProvider{
+		config:    config,
+		dnsClient: dnsClient,
+	}, nil
+}
+
+// Present creates a TXT rrset to fulfill DNS-01 challenge.
+func (d *DNSProvider) Present(zoneName, fqdn, value string) error {
+	ctx := context.Background()
+	zone, err := internal.GetZoneByName(ctx, d.dnsClient, zoneName)
+	if err != nil {
+		return fmt.Errorf("get zone by name: %w", err)
+	}
+	rrset, err := internal.GetRrsetByNameAndType(ctx, d.dnsClient, zone.ID, fqdn, string(domainsV2.TXT))
+	if err != nil && !errors.Is(err, internal.ErrRrsetNotFound) {
+		return fmt.Errorf("get rrset by name: %w", err)
+	}
+	content := fmt.Sprintf("\"%s\"", value)
+	if errors.Is(err, internal.ErrRrsetNotFound) {
+		createRrsetOpts := &domainsV2.RRSet{
+			Name: fqdn,
+			TTL:  d.config.TTL,
+			Records: []domainsV2.RecordItem{
+				{Content: content},
+			},
+			Type: domainsV2.TXT,
+		}
+		_, err = d.dnsClient.CreateRRSet(ctx, zone.ID, createRrsetOpts)
+		if err != nil {
+			return fmt.Errorf("create new rrset: %w", err)
+		}
+	} else {
+		record := domainsV2.RecordItem{
+			Content: content,
+		}
+		rrset.Records = append(rrset.Records, record)
+		updateRrsetOpts := &domainsV2.RRSet{
+			TTL:     rrset.TTL,
+			Records: rrset.Records,
+			Type:    domainsV2.TXT,
+		}
+		err = d.dnsClient.UpdateRRSet(ctx, zone.ID, rrset.ID, updateRrsetOpts)
+		if err != nil {
+			return fmt.Errorf("added record to existsing rrset: %w", err)
 		}
 	}
 
-	return lastErr
+	return nil
 }
 
-func unFQDN(name string) string {
-	n := len(name)
-	if n != 0 && name[n-1] == '.' {
-		return name[:n-1]
-	}
-	return name
-}
-
-func getEnvOrDefaultString(envVar, defaultValue string) string {
-	v := os.Getenv(envVar)
-	if v == "" {
-		return defaultValue
-	}
-	return v
-}
-
-func getEnvOrErrorString(envVar string) (string, error) {
-	v := getEnvOrDefaultString(envVar, "")
-	if v == "" {
-		return "", fmt.Errorf("%s is missing", envVar)
-	}
-	return v, nil
-}
-
-func getEnvOrDefaultInt(envVar string, defaultValue int) int {
-	v, err := strconv.Atoi(os.Getenv(envVar))
+// CleanUp removes a content from TXT rrset used for DNS-01 challenge.
+func (d *DNSProvider) CleanUp(zoneName, fqdn, value string) error {
+	ctx := context.Background()
+	zone, err := internal.GetZoneByName(ctx, d.dnsClient, zoneName)
 	if err != nil {
-		return defaultValue
+		return fmt.Errorf("get zone by name: %w", err)
 	}
-	return v
+	rrset, err := internal.GetRrsetByNameAndType(ctx, d.dnsClient, zone.ID, fqdn, string(domainsV2.TXT))
+	if err != nil {
+		return fmt.Errorf("get rrset by name and type: %w", err)
+	}
+	if len(rrset.Records) == 1 {
+		err = d.dnsClient.DeleteRRSet(ctx, zone.ID, rrset.ID)
+		if err != nil {
+			return fmt.Errorf("delete rrset: %w", err)
+		}
+	} else {
+		newRecords := []domainsV2.RecordItem{}
+		for i := range rrset.Records {
+			if rrset.Records[i].Content != fmt.Sprintf("\"%s\"", value) {
+				newRecords = append(newRecords, rrset.Records[i])
+			}
+		}
+		err = d.dnsClient.UpdateRRSet(ctx, zone.ID, rrset.ID, &domainsV2.RRSet{
+			TTL:     rrset.TTL,
+			Records: newRecords,
+		})
+		if err != nil {
+			return fmt.Errorf("delete one record from rrset: %w", err)
+		}
+	}
+
+	return nil
 }
 
-func getEnvOrDefaultSecond(envVar string, defaultValue int) time.Duration {
-	return time.Duration(getEnvOrDefaultInt(envVar, defaultValue)) * time.Second
+func getDNSClientFromConfig(config *Config) (domainsV2.DNSClient[domainsV2.Zone, domainsV2.RRSet], error) {
+	ctx := context.Background()
+	options := &selvpcclient.ClientOptions{
+		Context:    ctx,
+		DomainName: config.CredentialsForDNS.AccountID,
+		Username:   config.CredentialsForDNS.Username,
+		Password:   config.CredentialsForDNS.Password,
+		ProjectID:  config.CredentialsForDNS.ProjectID,
+	}
+
+	client, err := selvpcclient.NewClient(options)
+	if err != nil {
+		return nil, fmt.Errorf("setup selvpc client: %w", err)
+	}
+
+	projectToken := client.GetXAuthToken()
+	hdrs := http.Header{}
+	hdrs.Add(headerForOSProjectToken, projectToken)
+	hdrs.Add("User-Agent", userAgent)
+
+	httpClient := &http.Client{
+		Timeout: time.Duration(config.HTTPTimeout) * time.Second,
+	}
+	domainsClient := domainsV2.NewClient(config.BaseURL, httpClient, hdrs)
+
+	return domainsClient, nil
 }
